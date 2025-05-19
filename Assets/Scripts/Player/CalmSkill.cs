ee0220a08fa8f35ce3dbf7a4707cc42983c41ea7
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-05-20 01:19:38 +0200

    Calm Skill reload time

--- Assets/Scripts/Player/CalmSkill.cs
+++ Assets/Scripts/Player/CalmSkill.cs
@@ -1,11 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class CalmSkill : MonoBehaviour
 {
 
-    [SerializeField] private float duration, elapsedTime;
+    [SerializeField] private float duration = 2.0f, elapsedTime, reloadTime = 5f;
     [SerializeField] private KeyCode activationkey = KeyCode.T;
     [SerializeField] private PlayerData pd;
+
+    IEnumerator SkillReload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        elapsedTime = 0;
+    }
 
     private void SkillTrigger()
     {
@@ -24,7 +31,8 @@ public class CalmSkill : MonoBehaviour
         if (elapsedTime >= duration)
         {
             pd.invulnerable = false;
-            elapsedTime = 0;
+            //elapsedTime = 0;
+            StartCoroutine(SkillReload());
         }
     }
 
