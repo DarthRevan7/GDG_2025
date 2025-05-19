bec3f1933df1ff02624d16bf9a283653d62cc921
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-05-19 10:42:13 +0200

    Nemico base

---
+++ Assets/Scripts/Enemy/EnemyAIBase.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemyAIBase : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 direction = new Vector3();
+    [SerializeField] private float aggroRange = 5f;
+    [SerializeField] private bool followingPlayer = false;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerData>().lives--;
+        }
+        else if (other.gameObject.GetComponent<Scream>() != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        if (followingPlayer)
+        {
+            transform.Translate(direction.normalized * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void CalculateDirection()
+    {
+        direction = transform.position - player.position;
+        followingPlayer = direction.magnitude <= aggroRange;
+    }
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateDirection();
+        FollowPlayer();
+    }
+}
