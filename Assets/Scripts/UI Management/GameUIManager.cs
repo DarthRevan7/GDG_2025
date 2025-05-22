9037b0ebedda0d6ffd6f26026aa00c3ac39de6df
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-05-22 12:02:52 +0200

    Level Management Completed!

--- Assets/Scripts/UI Management/GameUIManager.cs
+++ Assets/Scripts/UI Management/GameUIManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class GameUIManager : MonoBehaviour
 {
     [SerializeField] private PlayerData pd;
     [SerializeField] private TMP_Text livesText, moneyText;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private LevelManager levelManager;
+
 
     void Awake()
     {
@@ -12,18 +17,36 @@ public class GameUIManager : MonoBehaviour
         livesText = GameObject.Find("LivesText").GetComponent<TMP_Text>();
         moneyText = GameObject.Find("MoneyText").GetComponent<TMP_Text>();
 
+        winPanel = transform.Find("WinPanel").gameObject;
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pd.lives >= 0) {
+        if (pd.lives >= 0)
+        {
             livesText.text = "Lives: " + pd.lives.ToString();
-        } else {
+        }
+        else
+        {
             livesText.text = "DEAD";
         }
 
         moneyText.text = "$$$$$: " + pd.money.ToString();
+
+        if (levelManager.levelWon)
+        {
+            winPanel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void LoadSceneIndexed(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 
 
